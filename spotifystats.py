aa4933f8f0818cd9e99ebaea3c1ce651ffc568f2
tommci <129996514+tommci@users.noreply.github.com>
2024-05-17 10:21:20 -0600

    Various changes
    -Removed playlist index tracking
    -Added album tracking
    -Added song length tracking
    -Changed file delimiter to a less common character
    -Implemented playlist change option
    -Added a confirmation before each time the menu prints
    -Menu now shows the playlist name
    -Various text formatting tweaks

--- spotifystats.py
+++ spotifystats.py
@@ -1,15 +1,18 @@
 import csv
 
 class Song:
-    def __init__(self, name, artistCount, artists, listens, location, plays):
+    def __init__(self, name, artistCount, artists, listens, album, plays, length):
         self.name = name
         self.artistCount = artistCount
         self.artists = artists
         self.listens = listens
-        self.location = location
+        self.album = album
         self.plays = plays
+        self.length = length
     def display(self):
-        print(f'\n{self.name}')
+        mins = int(self.length) // 60
+        secs = int(self.length) % 60
+        print(f'\n{self.name} ({mins}:{secs})')
         i = 0
         while(i < int(self.artistCount)):
             if(i == int(self.artistCount) - 1):
@@ -17,7 +20,7 @@ class Song:
             else:
                 print(f'{self.artists[i]}, ', end='')
             i += 1
-        print(f'\nGlobal Listens: {self.listens}\nPlaylist Location: {self.location}\nTimes Played: {self.plays}')   
+        print(f'\n{self.album}\nGlobal Listens: {self.listens}\nTimes Played: {self.plays}')   
 
 def open_playlist(name):
     playlist = []
@@ -27,21 +30,20 @@ def open_playlist(name):
         print(f'Playlist not found, creating new playlist file.')
         open(f'spotify-shuffle/{name}.txt', 'x', encoding='utf-8')
     with open(f'spotify-shuffle/{name}.txt', 'r', encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter=',')
+        reader = csv.reader(file, delimiter='|')
         for line in reader:
             tempName = line[0]
             artCount = line[1]
             tempArtists = []
             j = 2
-            c = 0
-            while(c < int(artCount)):
+            while(j < int(artCount) + 2):
                 tempArtists.append(line[j])
-                c += 1
                 j += 1
             tempListens = line[j]
-            tempLocation = line[j + 1]
+            tempAlbum = line[j + 1]
             tempPlays = line[j + 2]
-            song = Song(tempName, artCount, tempArtists, tempListens, tempLocation, tempPlays)
+            tempLength = line[j + 3]
+            song = Song(tempName, artCount, tempArtists, tempListens, tempAlbum, tempPlays, tempLength)
             playlist.append(song)
     return playlist
 
@@ -66,24 +68,21 @@ def add_song(oldPlaylist):
             tempArtName = input(f'ARTIST {i + 1}> ')
             tempArtists.append(tempArtName)
             i += 1
+        print(f'Enter the minutes and seconds of the song:')
+        mins = int(input('MINUTES> '))
+        secs = int(input('SECONDS> '))
+        print(f'Enter the album the song is on:')
+        tempAlbum = input('ALBUM> ')
         print(f'Enter the number of global listens:')
         tempListens = input('LISTENS> ')
-        print(f'Enter the location (index) of the song on the playlist:')
-        tempLocation = input('LOCATION> ')
-        print(f'\nDoes this look right? (Y/N)\nName: {tempName}\nArtists: ', end='')
-        i = 0
-        while(i < artCount):
-            if(i == artCount - 1):
-                print(f'{tempArtists[i]}', end='')
-            else:
-                print(f'{tempArtists[i]}, ', end='')
-            i += 1
-        print(f'\nGlobal Listens: {tempListens}\nPlaylist Location: {tempLocation}')
+        tempLength = (mins * 60) + secs
+        song = Song(tempName, artCount, tempArtists, tempListens, tempAlbum, 1, tempLength)
+        print(f'\nDoes this look right? (Y/N)')
+        song.display()
         choice = input('Y/N> ')
         if(choice == 'Y' or choice == 'y'):
             loop = False
     print(f'Song added.')
-    song = Song(tempName, artCount, tempArtists, tempListens, tempLocation, 1)
     newPlaylist.append(song)
     return newPlaylist
 
@@ -97,6 +96,7 @@ def increment_song(oldPlaylist, song):
     return newPlaylist
 
 def display_songs(playlist):
+    print(f'\nSonglist:')
     for song in playlist:
         song.display()
 
@@ -110,12 +110,12 @@ def search_for_song(playlist, songToFind):
 def update_file(playlist, name):
     with open(f'spotify-shuffle/{name}.txt', 'w', encoding='utf-8') as file:
         for song in playlist:
-            file.write(f'{song.name},{song.artistCount},')
+            file.write(f'{song.name}|{song.artistCount}|')
             i = 0
             while(i < int(song.artistCount)):
-                file.write(f'{song.artists[i]},')
+                file.write(f'{song.artists[i]}|')
                 i += 1
-            file.write(f'{song.listens},{song.location},{song.plays}\n')
+            file.write(f'{song.listens}|{song.album}|{song.plays}|{song.length}\n')
 
 # end of definitions
 
@@ -123,9 +123,8 @@ print(f'Enter playlist name:')
 playlistName = input('NAME> ')
 playlist = open_playlist(playlistName)
 
-loop = True
-while(loop):
-    print(f'\n~~~~~~~~~~\nChoose an option.\nIf you\'re finished, please use the quit option!\n1) Increment Song\n2) Add Song\n3) Change Playlist\n4) List Songs\n5) Quit')
+while(True):
+    print(f'~~~~~~~~~~\nCurrent Playlist: {playlistName}\n~~~~~~~~~~\nChoose an option.\nIf you\'re finished, please use the quit option!\n1) Increment Song\n2) Add Song\n3) Change Playlist\n4) List Songs\n5) Quit')
     option = input('CHOICE> ')
 
     match option:
@@ -153,13 +152,17 @@ while(loop):
         case '2':
             playlist = add_song(playlist)
         case '3':
-            print(f'This doesn\'t do anything yet.')
+            print(f'\nEnter playlist name:')
+            playlistName = input('NAME> ')
+            playlist = open_playlist(playlistName)
         case '4':
-            print(f'\nSonglist:')
             display_songs(playlist)
         case '5':
-            loop = False
+            update_file(playlist, playlistName)
+            break
         case _:
             print(f'Invalid choice selected.')
 
     update_file(playlist, playlistName)
+    print(f'\nPress enter to continue.')
+    input()
